05c3628f466cd02b9f2776d71f9fa4bac0c7ea4b
M0staphaTaha <darsh.taha12382@gmail.com>
2024-02-28 18:43:43 +0200

    fav

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -55,7 +55,7 @@ const ProductItem = ({ id, price, name, imgUrl, discription, Fulldiscription }) 
                             </div>
                             <div className='flex items-center translate-y-1'>
                                 <button onClick={toggleNavbar} >
-                                    {isOpen ? <img className='w-[37px]' src="/public/images/heart.svg" alt="heart" /> : <img className='w-[37px]' src='/images/fav.svg' alt="fav" />}
+                                    {isOpen ? <svg className="h-8 w-8 text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" /></svg> : <img className='w-[37px]' src='/images/fav.svg' alt="fav" />}
                                 </button>
                             </div>
                         </div>
